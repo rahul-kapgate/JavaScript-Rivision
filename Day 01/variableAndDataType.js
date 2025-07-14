9918c7873a8a577d31ee248c8501583a1c7ae346
Rahul Kapagte <rahulkapgateyt@gmail.com>
2025-07-14 14:42:34 +0530

    Add initial JavaScript files and .gitignore
    - Created .gitignore to exclude node_modules, package files, and user input scripts.
    - Added variableAndDataType.js with examples of variable declarations and data types.
    - Introduced Operators.js demonstrating various arithmetic and logical operations.

---
+++ Day 01/variableAndDataType.js
@@ -0,0 +1,49 @@
+//Variable and data types
+
+// 1.
+const number = 10
+let name = "Deva"
+
+console.log(number)
+console.log(name)
+
+//2
+const flag = true
+
+console.log(flag)
+
+//3
+let myName = ""
+let age = 12
+let decimal = 1.1
+let test = []
+let myObj = {}
+
+
+console.log(typeof myName)
+console.log(typeof age)
+console.log(typeof decimal)
+console.log(typeof myObj)
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof true)
+
+
+//4
+let myCity = "Bnagalore"
+
+console.log(myCity)
+
+myCity = "Nagpur"
+
+console.log(myCity)
+
+//5
+const myAge = 23
+
+console.log(myAge)
+
+// myAge = 24
+// console.log(myAge)
+//TypeError: Assignment to constant variable.
+
---
+++ Day 02/Operators.js
@@ -0,0 +1,41 @@
+// Operators
+
+// 1
+console.log(10 + 20)
+console.log(10 - 20)
+console.log(10 * 20)
+console.log(10 / 2)
+console.log(10 % 3)
+
+console.log(2**2)
+
+// 2
+let myAge = 20
+myAge += 3
+console.log(myAge)
+
+myAge -= 1
+console.log(myAge)
+
+//3
+console.log(10 > 12)
+console.log(10 < 12)
+
+console.log(10 > 10 )
+console.log(10 >= 10)
+
+console.log("/n")
+
+console.log(10 == "10")
+console.log(10 === "10")
+
+//4
+console.log("/n");
+console.log(!true)
+console.log(false && true)
+console.log(false || true)
+
+//5
+console.log("/n");
+const num = -10
+num > 0 ? console.log(true) : console.log(!true);
